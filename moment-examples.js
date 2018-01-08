235d6e5e551b347c9591fd648a79ab5aee727003
Blarcmann <dtl121ois@gmail.com>
2018-01-08 01:55:31 +0100

    Used moment, unixCode, timestamp...

---
+++ moment-examples.js
@@ -0,0 +1,16 @@
+var moment = require('moment');
+var now = moment();
+
+//console.log(now.format());
+//console.log(now.format('X'));
+//console.log(now.valueOf());
+
+var timestamp = 1515372268027;
+var timestampMoment = moment.utc(timestamp);
+console.log(timestampMoment.local().format('h:mm a'));
+
+
+//console.log(now.format());
+//
+//console.log(now.format("MMMM Do YYYY, h:mm a")); 
+//// h-12-hours, H-24-hours,  m-munites, A/a-AM/pm, DDDD-fullday, MMMM-fullmonth, YYYY-fullyear 
